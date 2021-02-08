c17597040cdd72ba0407798a30e48a9f99a5bdc0
Nadezhda <hopeline@mail.bg>
2021-02-08 14:15:26 +0200

    Updates with Reference

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,79 +1,85 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Warehouse {
+    public static final int COUNT = 0;
+    public static final int ITEM_NAME = 2;
+    public static final int EXPIRE_DATE = 4;
+    public static final int DATE_OF_ENTRY = 6;
+    public static final int MANUFACTURE_NAME = 8;
+    public static final int ITEM_UNIT = 10;
+    public static final int ITEM_QUANTITY = 12;
+    public static final int COMMENTS = 14;
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int optional;
         int count = 1;
+        String[] warehouseArray = {" ", "\t\t", " ", "\t\t", " ", "\t\t", "  ", "\t\t", "  ", "\t\t", "  ", "\t\t", " ", "\t\t", " "};
+        String[][] warehouseMatrix = {
+                {"No Place: ", "\t\t", "Name of item", "\t\t", "Date of expire", "\t\t", "Date of entry ", "\t\t", "Manufacture's Name", "\t\t\t", "Item unit", "\t\t\t", "Quantity", "\t\t\t", "Comments"},
+                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " ", "\t\t\t\t", " "},
+                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " ", "\t\t\t\t", " "},
+                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " ", "\t\t\t\t", " "},
+                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " ", "\t\t\t\t", " "},
+                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " ", "\t\t\t\t", " "},
+        };
 
         do {
             menu();
             optional = input.nextInt();
             switch (optional) {
                 case 1:
-                    fillMatrix(count);
+                    warehouseArray[COUNT] = Integer.toString(count);
+                    warehouseArray[ITEM_NAME] = addItemName();
+                    warehouseArray[EXPIRE_DATE] = addExpireDate();
+                    warehouseArray[DATE_OF_ENTRY] = addDateOfEntry();
+                    warehouseArray[MANUFACTURE_NAME] = addManufactureName();
+                    warehouseArray[ITEM_UNIT] = addItemUnit();
+                    warehouseArray[ITEM_QUANTITY] = addItemQuantity();
+                    warehouseArray[COMMENTS] = addComments();
+
+                    printArray(warehouseArray);
+                    System.out.println();
+
+                    if (!checkExistingItemName(warehouseMatrix, warehouseArray, count) || !checkExpireDate(warehouseMatrix, warehouseArray, count)) {
+                        warehouseMatrix[count][Warehouse.COUNT] = warehouseArray[Warehouse.COUNT];
+                        warehouseMatrix[count][ITEM_NAME] = warehouseArray[ITEM_NAME];
+                        warehouseMatrix[count][EXPIRE_DATE] = warehouseArray[EXPIRE_DATE];
+                        warehouseMatrix[count][DATE_OF_ENTRY] = warehouseArray[DATE_OF_ENTRY];
+                        warehouseMatrix[count][MANUFACTURE_NAME] = warehouseArray[MANUFACTURE_NAME];
+                        warehouseMatrix[count][ITEM_UNIT] = warehouseArray[ITEM_UNIT];
+                        warehouseMatrix[count][ITEM_QUANTITY] = warehouseArray[ITEM_QUANTITY];
+                        warehouseMatrix[count][COMMENTS] = warehouseArray[COMMENTS];
+                    } else {
+                        checkExistingItem(warehouseMatrix, warehouseArray, count);
+                        count--;
+                    }
+
+                    printMatrix(warehouseMatrix, warehouseArray);
                     count++;
+
+                    if (count == warehouseMatrix.length) {
+                        System.out.println("Warehouse is full");
+                    }
                     break;
                 case 2:
-                    //   referenceItemByDate(warehouseMatrix);
+                    printReferenceByDate(warehouseMatrix, warehouseArray);
                     break;
                 default:
                     break;
             }
-        } while (optional != 0);
+        } while ((optional != 0) || (count < warehouseMatrix.length));
     }
 
     public static void menu() {
         System.out.println();
-        System.out.println("1 - Add new Item");
+        System.out.println("1 - Add new Item. Max quantity are 100.");
         System.out.println("2 - Reference for availability in a given period");
         System.out.println("0 - Exit");
     }
-
-    public static void fillMatrix(int count) {
-        count = 1;
-        String[][] warehouseMatrix = {
-                {"No Place: ", "\t\t", "Name of item", "\t\t", "Date of expire", "\t\t", "Date of entry ", "\t\t", "Manufacture's Name", "\t\t\t", "Item unit", "\t\t\t", "Quantity"},
-                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "},
-                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "},
-                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "},
-                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "},
-                {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "},
-        };
-
-        for (int i = 0; i < warehouseMatrix.length; i++) {
-
-            String[] warehouseArray = {" ", "\t\t\t\t", " ", "\t\t\t\t", " ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", "  ", "\t\t\t\t", " "};
-            warehouseArray[0] = Integer.toString(count);
-            warehouseArray[2] = addItemName();
-            warehouseArray[4] = addExpireDate();
-            warehouseArray[6] = addDateOfEntry();
-            warehouseArray[8] = addManufactureName();
-            warehouseArray[10] = addItemUnit();
-            warehouseArray[12] = addItemQuantity();
-
-            printArray(warehouseArray);
-            System.out.println();
-
-            if (!checkExistingItemName(warehouseMatrix, warehouseArray, count) || !checkExpireDate(warehouseMatrix, warehouseArray, count)) {
-                warehouseMatrix[count][0] = warehouseArray[0];
-                warehouseMatrix[count][2] = warehouseArray[2];
-                warehouseMatrix[count][4] = warehouseArray[4];
-                warehouseMatrix[count][6] = warehouseArray[6];
-                warehouseMatrix[count][8] = warehouseArray[8];
-                warehouseMatrix[count][10] = warehouseArray[10];
-                warehouseMatrix[count][12] = warehouseArray[12];
-            } else {
-                checkExistingItem(warehouseMatrix, warehouseArray, count);
-            }
-
-            printMatrix(warehouseMatrix, warehouseArray);
-            count++;
-        }
-        System.out.println("Warehouse is full");
-    }
-
 
     public static void printMatrix(String[][] warehouseMatrix, String[] warehouseArray) {
         for (int i = 0; i < warehouseMatrix.length; i++) {
@@ -165,5 +171,46 @@ public class Warehouse {
         String itemQuantity = input.nextLine();
         return itemQuantity;
     }
+
+    private static String addComments() {
+        System.out.println("Enter comments: ");
+        Scanner input = new Scanner(System.in);
+        String comments = input.nextLine();
+        return comments;
+    }
+
+    private static void printReferenceByDate(String[][] warehouseMatrix, String[] warehouseArray) {
+        for (int i = enterStartDate(warehouseMatrix); i <= enterEndDate(warehouseMatrix); i++) {
+            for (int j = 0; j < warehouseArray.length; j++) {
+                System.out.print(warehouseMatrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int enterStartDate(String[][] warehouseMatrix) {
+        System.out.println("Enter start date for reference: ");
+        Scanner input = new Scanner(System.in);
+        String startDate = input.nextLine();
+        for (int i = 1; i < warehouseMatrix.length; i++) {
+            if (warehouseMatrix[i][6].equalsIgnoreCase(startDate)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static int enterEndDate(String[][] warehouseMatrix) {
+        System.out.println("Enter end date for reference: ");
+        Scanner input = new Scanner(System.in);
+        String endDate = input.nextLine();
+        for (int i = enterStartDate(warehouseMatrix); i < warehouseMatrix.length; i++) {
+            if (warehouseMatrix[i][6].equalsIgnoreCase(endDate)) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
 }
 
